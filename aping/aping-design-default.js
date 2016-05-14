00a84d783e80a04df47b74f367f102c49b45fb98
César Castillo <cesar.castillo@planleft.com>
2016-05-14 13:51:28 -0500

    8.x-dev fixing image paths

--- aping/aping-design-default.js
+++ aping/aping-design-default.js
@@ -23,7 +23,7 @@ angular.module('jtt_aping_design_default', ['wu.masonry', 'linkify', 'angularMom
                 case "tumblr":
                 case "rss":
                 case "bandsintown":
-                    return "/sites/all/modules/aping/aping/img/"+_platform+".png";
+                    return "/modules/custom/aping/aping/img/"+_platform+".png";
             }
 
             return false;
